5cb47eccbc2fdb1fdd0a897ad7902ab828913f21
Salvatore Esposito <s.esposito@greennetwork.it>
2019-01-29 18:36:29 +0100

    slider funzionante

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,55 +1,57 @@
 import React from 'react';
-import Swiper from 'swiper';
-
-var mySwiper = new Swiper('.swiper-container', { 
-    
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-
-
-
-});
-
+import Swiper from 'react-id-swiper';
 
 export default class Carousel extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.goNext = this.goNext.bind(this)
+        this.goPrev = this.goPrev.bind(this)
+        this.swiper = null
+    }
+
+    goNext() {
+        if (this.swiper) this.swiper.slideNext()
+    }
+
+    goPrev() {
+        if (this.swiper) this.swiper.slidePrev()
+    }
 
     render() {
-        return <div class="swiper-container">
 
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide">Slide 1</div>
-                            <div class="swiper-slide">Slide 2</div>
-                            <div class="swiper-slide">Slide 3</div>
-                        </div>
-                        
+        const setting = {
+            effect: 'coverflow',
+            coverflowEffect: {
+                rotate: 80,
+                slideShadows: true,
+                depth: 150,
+                stretch: 30
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+                clickable: true
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+            },
+            renderPrevButton: () => <button className="swiper-button-prev">Prev</button>,
+            renderNextButton: () => <button className="swiper-button-next">Next</button>,
+        };
 
-                        <div class="swiper-pagination"></div>
+        return (
+            <div>
+            <Swiper className="swiper-container" {...setting} >
 
-
-                        <div class="swiper-button-prev"></div>
-                        <div class="swiper-button-next"></div>
-
-
-                        <div class="swiper-scrollbar"></div>
-
-                </div>
+                    <div className="swiper-slide" style={{backgroundImage:"url(https://via.placeholder.com/800x800.png?text=Film1)"}}></div>
+                    <div className="swiper-slide" style={{ backgroundImage: "url(https://via.placeholder.com/800x800.png?text=Film2)" }}></div>
+                    <div className="swiper-slide" style={{ backgroundImage: "url(https://via.placeholder.com/800x800.png?text=Film3)" }}></div>
+                            
+            </Swiper>
+            </div>
+        );
     }
 }
+
