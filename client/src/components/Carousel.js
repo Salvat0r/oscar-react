15850b858cfbfbe68b95b253e40eac39cba6979c
Salvatore Esposito <s.esposito@greennetwork.it>
2019-01-29 16:22:25 +0100

    babel install

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import Swiper from 'swiper';
+
+var mySwiper = new Swiper('.swiper-container', { 
+    
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+
+
+});
+
 
 export default class Carousel extends React.Component {
 
 
     render() {
-        return <div></div>
+        return <div class="swiper-container">
+
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">Slide 1</div>
+                            <div class="swiper-slide">Slide 2</div>
+                            <div class="swiper-slide">Slide 3</div>
+                        </div>
+                        
+
+                        <div class="swiper-pagination"></div>
+
+
+                        <div class="swiper-button-prev"></div>
+                        <div class="swiper-button-next"></div>
+
+
+                        <div class="swiper-scrollbar"></div>
+
+                </div>
     }
 }
